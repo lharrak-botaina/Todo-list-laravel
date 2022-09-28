014ab09f369e25eb8d90a012473318b184efd2e7
lharrak-botaina <lharrak.botaina@gmail.com>
2022-09-28 12:53:58 +0100

    initialisation

---
+++ laravel/resources/views/add.blade.php
@@ -0,0 +1,27 @@
+<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.bundle.min.js">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
+<div class="page-content page-container" id="page-content">
+    <div class="padding">
+        <div class="row container d-flex justify-content-center">
+<div class="col-md-6 col-lg-4">
+            <form class="card">
+              <h5 class="card-title fw-400">Tasks</h5>
+
+              <div class="card-body">
+                <div class="form-group">
+                  <input class="form-control" type="text" placeholder="Task name">
+                </div>
+                <div class="form-group">
+                  <input class="form-control" type="text" placeholder="Description">
+                </div>
+
+                
+                <button class="btn btn-block btn-bold btn-primary">ADD</button>
+              </div>
+            </form>
+          </div>
+           </div>
+            </div>
+             </div>
